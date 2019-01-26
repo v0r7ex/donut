179f66c95393f6598221507013b1f9796f37500a
Violeta Druga <vdruga@e1z2r8p36.42.us.org>
2019-01-26 12:05:58 -0800

    game state displays

--- game.py
+++ game.py
@@ -7,16 +7,30 @@ class Game():
         self.resize_images()
         
     def load_images(self):
-        pass
+        self.bkgd_img = loadImage("white.jpg")
+        self.game_img = loadImage("game.png")
+        self.menu_img = loadImage("menu.png")
+        self.menu_hover_img = loadImage("menu_hover.png")
         
     def resize_images(self):
-        pass
-    
-    def create_buttons(self):
-        pass
+        self.bkgd_img.resize(width, height)
+        self.game_img.resize(width/3, width/6)
+        self.menu_img.resize(self.menu_button.w, self.menu_button.h)
+        self.menu_hover_img.resize(self.menu_button.w, self.menu_button.h)
         
+    def create_buttons(self):
+        self.menu_button = Button(50, 50, 28, 5, self.menu_img, self.menu_hover_img)
+
     def display(self):
-        pass
+        imageMode(CORNER)
+        image(self.bkgd_img, 0, 0)
+        imageMode(CENTER)
+        image(self.game_img, width/2, height/6)
+        self.menu_button.display()
+        
         
     def click(self):
-        pass
+        if self.menu_button.is_hovering():
+            return "menu"
+        else:
+            return None
--- help.py
+++ help.py
@@ -7,16 +7,30 @@ class Help():
         self.resize_images()
         
     def load_images(self):
-        pass
+        self.bkgd_img = loadImage("white.jpg")
+        self.help_img = loadImage("help.png")
+        self.menu_img = loadImage("menu.png")
+        self.menu_hover_img = loadImage("menu_hover.png")
         
     def resize_images(self):
-        pass
-    
-    def create_buttons(self):
-        pass
+        self.bkgd_img.resize(width, height)
+        self.help_img.resize(int(width/2.5), width/13)
+        self.menu_img.resize(self.menu_button.w, self.menu_button.h)
+        self.menu_hover_img.resize(self.menu_button.w, self.menu_button.h)
         
+    def create_buttons(self):
+        self.menu_button = Button(50, 50, 28, 5, self.menu_img, self.menu_hover_img)
+
     def display(self):
-        pass
+        imageMode(CORNER)
+        image(self.bkgd_img, 0, 0)
+        imageMode(CENTER)
+        image(self.help_img, width/2, height/6)
+        self.menu_button.display()
+        
         
     def click(self):
-        pass
+        if self.menu_button.is_hovering():
+            return "menu"
+        else:
+            return None

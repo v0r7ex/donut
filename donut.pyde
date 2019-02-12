7c4a7a464e225f8247a1f1824bc0c44e7146b49b
Violeta <violetadruga@gmail.com>
2019-02-12 11:24:16 -0800

    audio handling, volume transitions

--- donut.pyde
+++ donut.pyde
@@ -6,14 +6,17 @@ def setup():
     global program_instance
     program_instance = Program()
     minim = Minim(this)
-    program_instance.menu_soundtrack = minim.loadFile("menu/smile.mp3")
-    program_instance.menu_soundtrack.play()
+    program_instance.menu_soundtrack = minim.loadFile("menu/smile_bensound.mp3")
+    program_instance.game_soundtrack = minim.loadFile("game/happy_life_fredji.mp3")
+    program_instance.current_soundtrack = program_instance.menu_soundtrack
+    program_instance.menu_soundtrack.loop()
     
     
 def draw():
     global program_instance
     background(50)
     program_instance.display()
+
     
 def mousePressed():
     global program_instance
--- program.py
+++ program.py
@@ -22,6 +22,25 @@ class Program():
                         "level_end": self.level_end
                         }
         self.gs = self.menu
+        self.menu_soundtrack = None #sounds loaded in setup()
+        self.game_soundtrack = None
+        self.current_soundtrack = None
+        
+    def check_soundtrack(self):
+        new_soundtrack = None
+        if self.gs == self.menu or self.gs == self.settings_state or self.gs == self.about:
+            if self.current_soundtrack is not self.menu_soundtrack:
+                new_soundtrack = self.menu_soundtrack
+        elif self.gs == self.game or self.gs == self.pause or self.gs == self.level_end:
+            if self.current_soundtrack is not self.game_soundtrack:
+                new_soundtrack = self.game_soundtrack
+        if new_soundtrack is not None: #shiftGain() gradually changes the volume
+            self.current_soundtrack.shiftGain(0, -50, 400)
+            delay(400)
+            self.current_soundtrack.pause()
+            self.current_soundtrack = new_soundtrack
+            self.current_soundtrack.loop()
+            self.current_soundtrack.shiftGain(-50, 0, 700)
         
     def display(self):
         self.gs.display()
@@ -31,3 +50,4 @@ class Program():
         if result is not None:
             self.gs = self.gs_dict[result]
             self.gs.reset()
+            self.check_soundtrack()
